f557eb26996a61aceac4bb30777f4c6f1e107253
Himanshi Khandelwal <himanshikhandelwal81@gmail.com>
2025-05-10 16:38:41 +0530

    add great-monday font in navbar title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,11 +14,11 @@ const Navbar = ({ darkMode, setDarkMode }) => {
       className={`${
         darkMode
           ? "bg-gray-800 text-white"
-          : "bg-gradient-to-r from-indigo-600 to-purple-600 text-white"
+          : "bg-gradient-to-r from-white to-gray-400 text-black"
       } p-4 shadow-lg`}
     >
       <div className="max-w-6xl mx-auto flex justify-between items-center">
-        <div className="text-2xl font-bold">
+        <div className="text-2xl font-bold font-greatMonday">
           <a href="/">My Portfolio</a>
         </div>
 
@@ -52,13 +52,9 @@ const Navbar = ({ darkMode, setDarkMode }) => {
         {/* Dark mode toggle button */}
         <button onClick={handleDarkModeToggle} className="text-white p-2">
           {darkMode ? (
-            <img src="/src/assets/light-mode.png"
-            width="40px"
-            height="40px" />
+            <img src="/src/assets/light-mode.png" width="40px" height="40px" />
           ) : (
-            <img src="/src/assets/dark-mode.png"
-            width="40px"
-            height="40px" />
+            <img src="/src/assets/dark-mode.png" width="40px" height="40px" />
           )}
         </button>
       </div>
